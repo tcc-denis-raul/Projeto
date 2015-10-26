a169f0df9b68fe231bc42ac43d2acd9557df4271
Denis Aoki 2 <denis2@Mac-76.local>
2015-10-26 05:49:40 -0200

    Menu modificado com dropdown e opção de listar todos os cursos adicionado. Fix #1

---
+++ Script/Conectar.php
@@ -0,0 +1,10 @@
+<!--Script para conectar ao Banco de Dados -->
+<?php
+	$servidor = "localhost";
+	$port = 5432;
+	$user = "postgres";
+	$password = "postgres";
+	$db = "paloma";
+	$dbconn = pg_connect("host=$servidor port=$port dbname=$db user=$user password=$password") or die('connection failed');
+?>
+
